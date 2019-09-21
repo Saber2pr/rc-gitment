cf97ac0962709f72b9d2677a6150f2322ab96816
saber2pr <saber2pr@gmail.com>
2019-09-21 13:21:53 +0000

    update

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,2 @@
 export * from "./useForceUpdate"
-export * from "./useIsMob"
-export * from "./useIsMobile"
 export * from "./usePreComp"
--- src/iconfont/index.tsx
+++ src/iconfont/index.tsx
@@ -1,12 +1,6 @@
 import React from "react"
 import "./iconfont.css"
 export namespace Icon {
-  export const Mao = () => <i className="iconfont icon-mao" />
-  export const Sousuo = () => <i className="iconfont icon-xiazai15" />
-  export const DarkTheme = () => <i className="iconfont icon-yejianmoshi" />
-  export const LightTheme = () => (
-    <i className="iconfont icon-baitianmoshimingliangmoshi" />
-  )
   export const Head = () => <i className="iconfont icon-icon26" />
   export const Delete = () => <i className="iconfont icon-shanchu" />
 }
