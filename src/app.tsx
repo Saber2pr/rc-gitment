e8f77fb20db9f8322d99039e1d68bc924e0dbfc3
Konpaku Youmu <saber2pr@gmail.com>
2020-03-29 13:24:11 +0800

    Update and rename src/app.tsx to  src/app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -5,8 +5,8 @@ import { Comments } from "./components"
 export const config = {
   username: "saber2pr",
   repo: "rc-gitment",
-  client_id: "4bc43492fa02848cee52",
-  client_secret: "679debf856ea289479cda611ce7f2e88607bc576"
+  client_id: "4bc4348cee52",
+  client_secret: "679debf856611c2e88607bc576"
 }
 
 ReactDOM.render(<Comments {...config} />, document.querySelector("#root"))
